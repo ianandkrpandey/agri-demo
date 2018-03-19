5ae02b11c6481500a4d09632b424345cfbf4f039
Anand Kumar <ianandkrpandey@users.noreply.github.com>
2018-03-20 01:26:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+function openSearch() {
+    document.getElementById("a").style.display = "block";
+}
+
+function closeSearch() {
+    document.getElementById("myOverlay").style.display = "none";
+}
+//dropdown
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+//Dropdown
---
+++ pdfFromHTML.js
@@ -0,0 +1,30 @@
+function HTMLtoPDF(){
+	console.log("akakakkakaka");
+var pdf = new jsPDF('p', 'pt', 'letter');
+source = $('#HTMLtoPDF')[0];
+specialElementHandlers = {
+	'#bypassme': function(element, renderer){
+		return true
+	}
+}
+margins = {
+    top: 50,
+    left: 60,
+    width: 545
+  };
+pdf.fromHTML(
+  	source // HTML string or DOM elem ref.
+  	, margins.left // x coord
+  	, margins.top // y coord
+  	, {
+  		'width': margins.width // max width of content on PDF
+  		, 'elementHandlers': specialElementHandlers
+  	},
+  	function (dispose) {
+  	  // dispose: object with X, Y of the last line add to the PDF
+			//          this allow the insertion of new lines after html
+			var customer = document.getElementById("myInput").value;
+        pdf.save(customer+'-.pdf');
+      }
+  )		
+}
